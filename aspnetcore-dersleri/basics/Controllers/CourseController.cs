5dbf4317cf42d843e8d29ec5565c7c456aee5b89
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-06-29 21:34:45 +0300

    Mvc/Routing

---
+++ aspnetcore-dersleri/basics/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+using basics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers;
+
+//course
+public class CourseController: Controller
+{
+    //course
+    //course/index
+    public IActionResult Index()
+    {
+        var kurs = new Course();
+
+        kurs.Id = 1;
+        kurs.Title = "Aspnet core kursu.";
+        kurs.Description = "Başarılı bir aspnet kursu.";
+        return View(kurs);
+    }
+
+    //course/list
+    public IActionResult List()
+    {
+        return View("CourseList");
+    }
+}
---
+++ aspnetcore-dersleri/basics/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers;
+
+//localhost            => home/index
+//localhost/home       => home/index
+//localhost/home/index => home/index
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+}
