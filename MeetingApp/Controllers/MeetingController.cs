89ccb503419ddd89e36847613bdb906654764d83
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-07-20 14:06:52 +0300

    MeetingApp Project
    Model Binding

--- MeetingApp/Controllers/MeetingController.cs
+++ MeetingApp/Controllers/MeetingController.cs
@@ -4,16 +4,26 @@ namespace MeetingApp.Controllers
 {
     public class MeetingController: Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View(); 
         }
-
+        
+        [HttpGet]
         public IActionResult Apply()
         {
             return View();
         }
+        
+        [HttpPost]
+        public IActionResult Apply(string Name, string Phone, string Email, bool WillAttend)
+        {
 
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult List()
         {
             return View();
