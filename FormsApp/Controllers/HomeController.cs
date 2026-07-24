125f3c326887847335b4673b2d7ac4455432a997
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-07-24 18:41:20 +0300

    Batch Update
    FormsApp projesine Batch Update eklenmesi.

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -140,7 +140,34 @@ public class HomeController : Controller
             return NotFound();
         }
 
+        return View("DeleteConfirm", entity);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id, int ProductId)
+    {
+        if(id != ProductId)
+        {
+            return NotFound();
+        }
+
+        var entity = Repository.Products.FirstOrDefault(p => p.ProductId == ProductId);
+        if(entity == null)
+        {
+            return NotFound();
+        }
+
         Repository.DeleteProduct(entity);
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult EditProducts(List<Product> Products)
+    {
+        foreach (var product in Products)
+        {
+            Repository.EditIsActive(product);
+        }
         return RedirectToAction("Index");
     }
 }
--- FormsApp/Models/Repository.cs
+++ FormsApp/Models/Repository.cs
@@ -6,17 +6,16 @@ namespace FormsApp.Models
         private static readonly List<Category> _categories = new();
         static Repository()
         {
-            _categories.Add(new Category {CategoryId =1, Name="Telefon"});
-            _categories.Add(new Category {CategoryId =2, Name="Bilgisayar"});
+            _categories.Add(new Category { CategoryId = 1, Name = "Telefon" });
+            _categories.Add(new Category { CategoryId = 2, Name = "Bilgisayar" });
 
-
-            _products.Add(new Product {ProductId=1, Name="Iphone 14", Price=40000, IsActive=true, Image="1.jpg", CategoryId=1});
-            _products.Add(new Product {ProductId=2, Name="Iphone 15", Price=50000, IsActive=true, Image="2.jpg", CategoryId=1});
-            _products.Add(new Product {ProductId=3, Name="Iphone 16", Price=60000, IsActive=true, Image="3.jpg", CategoryId=1});
-            _products.Add(new Product {ProductId=4, Name="Iphone 17", Price=70000, IsActive=true, Image="4.jpg", CategoryId=1});
-
-            _products.Add(new Product {ProductId=5, Name="Macbook Air", Price=80000, IsActive=true, Image="5.jpg", CategoryId=2});
-            _products.Add(new Product {ProductId=6, Name="Macbook Pro", Price=90000, IsActive=true, Image="6.jpg", CategoryId=2});
+            _products.Add(new Product {ProductId = 1, Name = "IPhone 14", Price = 40000, IsActive = true,Image = "1.jpg",CategoryId = 1 });
+            _products.Add(new Product {ProductId = 2, Name = "IPhone 15", Price = 50000, IsActive = false,Image = "2.jpg",CategoryId = 1 });
+            _products.Add(new Product {ProductId = 3, Name = "IPhone 16", Price = 60000, IsActive = true,Image = "3.jpg",CategoryId = 1 });
+            _products.Add(new Product {ProductId = 4, Name = "IPhone 17", Price = 70000, IsActive = false,Image = "4.jpg",CategoryId = 1 });
+            
+            _products.Add(new Product {ProductId = 5, Name = "Macbook Air", Price = 80000, IsActive = false,Image = "5.jpg",CategoryId = 2 });
+            _products.Add(new Product {ProductId = 6, Name = "Macbook Pro", Price = 90000, IsActive = true,Image = "6.jpg",CategoryId = 2 });
         }
 
         public static List<Product> Products
@@ -32,25 +31,38 @@ namespace FormsApp.Models
             _products.Add(entity);
         }
 
-        public static void EditProduct(Product updateProduct)
+        public static void EditProduct(Product updatedProduct)
         {
-            var entity = _products.FirstOrDefault(p=> p.ProductId == updateProduct.ProductId);
+            var entity = _products.FirstOrDefault(p => p.ProductId == updatedProduct.ProductId);
 
-            if(entity != null)
+            if(entity != null) 
             {
-                entity.Name = updateProduct.Name;
-                entity.Price = updateProduct.Price;
-                entity.Image = updateProduct.Image;
-                entity.CategoryId = updateProduct.CategoryId;
-                entity.IsActive = updateProduct.IsActive;
+                if(!string.IsNullOrEmpty(updatedProduct.Name)) 
+                {
+                    entity.Name = updatedProduct.Name;
+                }
+                entity.Price = updatedProduct.Price;
+                entity.Image = updatedProduct.Image;
+                entity.CategoryId = updatedProduct.CategoryId;
+                entity.IsActive = updatedProduct.IsActive;
+            }
+        }
+
+        public static void EditIsActive(Product updatedProduct)
+        {
+            var entity = _products.FirstOrDefault(p => p.ProductId == updatedProduct.ProductId);
+
+            if(entity != null) 
+            {
+                entity.IsActive = updatedProduct.IsActive;
             }
         }
 
         public static void DeleteProduct(Product deletedProduct)
         {
-            var entity = _products.FirstOrDefault(p=> p.ProductId == deletedProduct.ProductId);
+            var entity = _products.FirstOrDefault(p => p.ProductId == deletedProduct.ProductId);
 
-            if(entity != null)
+            if(entity != null) 
             {
                 _products.Remove(entity);
             }
