6a5d9e08b088be9126861d2de3c4e534fea8dd70
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-07-28 13:53:20 +0300

    Course List
    Kurs Listesinin oluşturulması.

--- efcoreApp/Controllers/KursController.cs
+++ efcoreApp/Controllers/KursController.cs
@@ -1,5 +1,6 @@
 using efcoreApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace efcoreApp.Controllers
@@ -10,6 +11,12 @@ namespace efcoreApp.Controllers
         public KursController(DataContext context)
         {
             _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var kurslar = await _context.Kurslar.ToListAsync();
+            return View(kurslar);
         }
 
         public IActionResult Create()
