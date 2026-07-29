52d0a020047d7d464f573929fdba36450d5895d3
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-07-29 11:58:14 +0300

    Compulsory Teacher Selection
    Yeni kurs kaydı eklenirken öğretmen seçiminin zorunlu kılınması entegre edildi.

--- efcoreApp/Controllers/KursController.cs
+++ efcoreApp/Controllers/KursController.cs
@@ -32,11 +32,16 @@ namespace efcoreApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Kurs model)
+        public async Task<IActionResult> Create(KursViewModel model)
         {
-            _context.Kurslar.Add(model);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
+            if(ModelState.IsValid)
+            {
+                _context.Kurslar.Add(new Kurs() {KursId= model.KursId, Baslik = model.Baslik, OgretmenId = model.OgretmenId});
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Ogretmenler = new SelectList(await _context.Ogretmenler.ToListAsync(), "OgretmenId", "AdSoyad");
+            return View(model);
         }
 
         [HttpGet]
@@ -99,6 +104,7 @@ namespace efcoreApp.Controllers
                 }
                 return RedirectToAction("Index");
             }
+            ViewBag.Ogretmenler = new SelectList(await _context.Ogretmenler.ToListAsync(), "OgretmenId", "AdSoyad");
             return View(model);
         }
 
