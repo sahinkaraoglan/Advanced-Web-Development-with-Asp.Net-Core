6af14fc570cf7f8ec2a0d66588bdfa26be3eec66
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-07-30 21:58:14 +0300

    Connection String
    Connection String oluşturulması.

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,5 +1,15 @@
+using BlogApp.Data.Concrete.EfCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<BlogContext>(options =>
+{
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("Data Source=blog.db");
+    options.UseSqlite(connectionString);
+});
 
 app.MapGet("/", () => "Hello World!");
 
