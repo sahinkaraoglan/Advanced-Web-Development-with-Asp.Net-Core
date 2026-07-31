2efdd8a2ea18529530f055a3990ebd1be0d4bb16
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-07-31 18:38:17 +0300

    Layout
    Layout sayfasının oluşturulması.

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddScoped<IPostRepository, EfPostRepository>();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 SeedData.TestVerileriniDoldur(app);
 
 app.MapDefaultControllerRoute();
