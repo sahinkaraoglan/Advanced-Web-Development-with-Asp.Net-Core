35e3223885c392ae39842f7ef4e5371a31d04834
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-08-04 22:57:07 +0300

    Listing Postal Records
    Postların kullanıcının rolüne göre listelenmesi.

--- BlogApp/Controllers/PostsController.cs
+++ BlogApp/Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using BlogApp.Data.Abstract;
 using BlogApp.Data.Concrete.EfCore;
 using BlogApp.Entity;
 using BlogApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -69,12 +70,14 @@ namespace BlogApp.Controllers
         }
 
         [HttpGet]
+        [Authorize] //bu actionnun çalışması için giriş kesinlikle yapılmalı.
         public IActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
+        [Authorize]
         public IActionResult Create(PostCreateViewModel model)
         {
             if(ModelState.IsValid)
@@ -95,6 +98,20 @@ namespace BlogApp.Controllers
                 return RedirectToAction("Index");
             }
             return View(model);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> List()
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier) ?? "");
+            var role = User.FindFirstValue(ClaimTypes.Role);
+
+            var posts = _postRepository.Posts;
+            if(string.IsNullOrEmpty(role))
+            {
+                posts = posts.Where(i => i.UserId == userId);
+            }
+            return View(await posts.ToListAsync());
         }       
     }
 }
