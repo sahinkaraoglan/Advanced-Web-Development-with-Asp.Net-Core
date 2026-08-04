2a4f3c3cfd21064fd875e8a18466ac834d75890b
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-08-04 18:39:57 +0300

    User Registration Form
    Kullanıcı kayıt sayfasının oluşturulması sağlandı.

--- BlogApp/Controllers/UsersController.cs
+++ BlogApp/Controllers/UsersController.cs
@@ -24,6 +24,23 @@ namespace BlogApp.Controllers
             return View();
         }
 
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+
         public async Task<ActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
