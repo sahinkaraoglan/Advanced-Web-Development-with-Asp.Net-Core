d9dc52e0b9feb65d2f1144ba57521cae12cfe87b
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-08-04 19:13:50 +0300

    Creating a User Account
    Yeni bir kullanıcı oluşturulması sağlandı.

--- BlogApp/Controllers/UsersController.cs
+++ BlogApp/Controllers/UsersController.cs
@@ -1,9 +1,11 @@
 using System.Security.Claims;
 using BlogApp.Data.Abstract;
+using BlogApp.Entity;
 using BlogApp.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Controllers
 {
@@ -14,34 +16,47 @@ namespace BlogApp.Controllers
         {
             _userRepository = userRepository;
         }
-
+       
         public IActionResult Login()
         {
             if(User.Identity!.IsAuthenticated)
             {
-                return RedirectToAction("Index", "Posts");
+                return RedirectToAction("Index","Posts");
             }
             return View();
         }
 
-
-        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
             if(ModelState.IsValid)
             {
-                return RedirectToAction("Login");
+                var user = await _userRepository.Users.FirstOrDefaultAsync(x => x.UserName == model.UserName || x.Email == model.Email);
+                if(user == null)
+                {
+                    _userRepository.CreateUser(new User {
+                        UserName  = model.UserName,
+                        Name = model.Name,
+                        Email = model.Email,
+                        Password = model.Password,
+                        Image = "avatar.jpg"
+                    });
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username ya da Email kullanımda.");
+                }
             }
             return View(model);
         }
 
-        public async Task<ActionResult> Logout()
+        public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login");
@@ -50,9 +65,10 @@ namespace BlogApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
-            if(ModelState.IsValid)
+            if(ModelState.IsValid)  
             {
-                var isUser = _userRepository.Users.FirstOrDefault( x => x.Email == model.Email && x.Password == model.Password);
+                var isUser = _userRepository.Users.FirstOrDefault(x => x.Email == model.Email && x.Password == model.Password);
+
                 if(isUser != null)
                 {
                     var userClaims = new List<Claim>();
@@ -65,32 +81,33 @@ namespace BlogApp.Controllers
                     if(isUser.Email == "info@sahinkaraoglan.com")
                     {
                         userClaims.Add(new Claim(ClaimTypes.Role, "admin"));
+                    } 
 
-                        var claimsIdentity = new ClaimsIdentity(userClaims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var claimsIdentity = new ClaimsIdentity(userClaims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                        var authProperties = new AuthenticationProperties
-                        {
-                            IsPersistent = true
-                        };
+                    var authProperties = new AuthenticationProperties 
+                    {
+                        IsPersistent = true
+                    };
 
-                        await HttpContext.SignOutAsync(     CookieAuthenticationDefaults.AuthenticationScheme
-                        );
+                    await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
-                        await HttpContext.SignInAsync(
-                            CookieAuthenticationDefaults.AuthenticationScheme, 
-                            new ClaimsPrincipal(claimsIdentity), authProperties
-                        );
+                    await HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        new ClaimsPrincipal(claimsIdentity), 
+                        authProperties);
 
-                        return RedirectToAction("Index", "Posts");
-                    }
+                    return RedirectToAction("Index","Posts");
                 }
                 else
                 {
                     ModelState.AddModelError("", "Kullanıcı adı veya şifre yanlış");
                 }
-            }
+            } 
             
             return View(model);
         }
+       
+       
     }
 }
