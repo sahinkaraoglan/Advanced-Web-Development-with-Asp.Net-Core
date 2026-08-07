086f9bc587df0136e61b0980ad1cffa2b1644bbd
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-08-07 16:32:14 +0300

    Additional User Information
    Ekstra kullanıcı bilgilerinin eklenmesi.

---
+++ IdentityApp/Models/AppRole.cs
@@ -0,0 +1,10 @@
+
+
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityApp.Models
+{
+    public class AppRole : IdentityRole
+    {
+    }
+}
--- IdentityApp/Program.cs
+++ IdentityApp/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<IdentityContext>(
     options => options.UseSqlite(builder.Configuration["ConnectionStrings:SQLite_Connection"]));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<IdentityContext>();
 
 builder.Services.Configure<IdentityOptions>(options =>
 {
@@ -22,7 +22,7 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequireDigit=false;
 
     options.User.RequireUniqueEmail = true;
-    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz";
+    //options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz";
 });
 
 var app = builder.Build();
