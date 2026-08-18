2b83479abfdb65ec05a9a37ce72ad4f904a2a100
Şahin Karaoğlan <115473570+sahinkaraoglan@users.noreply.github.com>
2026-08-18 15:44:43 +0300

    Data Seeding
    Test verilerinin eklenmesi.

--- StoreApp/StoreApp.Web/Program.cs
+++ StoreApp/StoreApp.Web/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<StoreDbContext>(options =>
 {
-    options.UseSqlite(builder.Configuration["ConnectionStrings:StoreDbConnection"]);
+    options.UseSqlite(builder.Configuration["ConnectionStrings:StoreDbConnection"], b => b.MigrationsAssembly("StoreApp.Web"));
 });
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
