b94554045c15bb43592653e490b33c656dc1bfbe
Şahin Karaoğlan <115473570+sahinkaraoglan@users.noreply.github.com>
2026-08-18 15:13:27 +0300

    Repository
    Repository oluşturuldu.

--- StoreApp/StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
+using StoreApp.Data.Abstract;
 
 namespace StoreApp.Web.Controllers;
 public class HomeController : Controller
 {
-    
+    private readonly IStoreRepository _storeRepository;
+    public HomeController(IStoreRepository storeRepository)
+    {
+        _storeRepository = storeRepository;
+    }
     public IActionResult Index() => View();
 }
--- StoreApp/StoreApp.Web/Program.cs
+++ StoreApp/StoreApp.Web/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using StoreApp.Data.Abstract;
 using StoreApp.Data.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddDbContext<StoreDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration["ConnectionStrings:StoreDbConnection"]);
 });
+
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
 var app = builder.Build();
 
