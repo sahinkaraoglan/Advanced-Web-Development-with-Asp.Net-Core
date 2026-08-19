74b6f1803e5f3b8cd3601fe25f4ee0a2b46b8c3f
Şahin Karaoğlan <115473570+sahinkaraoglan@users.noreply.github.com>
2026-08-19 12:59:58 +0300

    Pagination
    Sayfalama eklendi.

--- StoreApp/StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -5,19 +5,26 @@ using StoreApp.Web.Models;
 namespace StoreApp.Web.Controllers;
 public class HomeController :Controller
 {
+    public int pageSize = 3;
     private IStoreRepository _storeRepository;
     public HomeController(IStoreRepository storeRepository)
     {
         _storeRepository = storeRepository;
     }
-    public IActionResult Index() {
+
+    //localhost:5000/?page=1
+    public IActionResult Index(int page = 1) {
       
-        var products = _storeRepository.Products.Select(p => new ProductViewModel {
-            Id = p.Id,
-            Name = p.Name,
-            Description = p.Description,
-            Price = p.Price
-        }).ToList();
+        var products = _storeRepository
+        .Products
+        .Skip((page - 1) * pageSize)  //(2-1)*3 = 3 //(3-1)*3 = 6
+        .Select(p => 
+            new ProductViewModel {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price
+            }).Take(pageSize);
 
         return View(new ProductListViewModel {
             Products = products
