b5f7cdebaf4abda5aa2a8bf966952581f7806d2c
Şahin Karaoğlan <115473570+sahinkaraoglan@users.noreply.github.com>
2026-08-19 15:07:26 +0300

    Applying the Template to the Project
    Şablonun projeye uygulanması.

--- StoreApp/StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -30,6 +30,7 @@ public class HomeController :Controller
             Products = products,
             PageInfo = new PageInfo {
                 ItemsPerPage = pageSize,
+                CurrentPage = page,
                 TotalItems = _storeRepository.Products.Count()
             }
         });
