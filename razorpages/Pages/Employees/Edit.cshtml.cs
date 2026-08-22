733bc15b90517c3dc82675518f67449e917f5c01
Şahin Karaoğlan <115473570+sahinkaraoglan@users.noreply.github.com>
2026-08-22 17:16:46 +0300

    Submitting the Form
    Formun post edilmesi gerçekleştirildi.

--- razorpages/Pages/Employees/Edit.cshtml.cs
+++ razorpages/Pages/Employees/Edit.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using razorpages.Models;
 using razorpages.Repository;
@@ -11,9 +12,23 @@ public class EditModel : PageModel
     {
         _employeeRepository = employeeRepository;
     }
+
+    [BindProperty]
     public Employee Employee { get; set; } = null!;
     public void OnGet(int id)
     {
         Employee = _employeeRepository.GetById(id);
+    }
+
+    // public IActionResult OnPost(Employee employee)
+    // {
+    //     Employee = _employeeRepository.Update(employee);
+    //     return RedirectToPage("/Employees/Index");
+    // }
+
+    public IActionResult OnPost()
+    {
+        Employee = _employeeRepository.Update(Employee);
+        return RedirectToPage("/Employees/Index");
     }
 }
--- razorpages/Repository/IEmployeeRepository.cs
+++ razorpages/Repository/IEmployeeRepository.cs
@@ -7,4 +7,6 @@ public interface IEmployeeRepository
     IEnumerable<Employee> GetAll();
 
     Employee GetById(int id);
+
+    Employee Update(Employee entity);
 }
