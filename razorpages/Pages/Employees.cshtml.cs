d254e8de07f1f1157bffa7442eeb0005e4c46deb
Şahin Karaoğlan <115473570+sahinkaraoglan@users.noreply.github.com>
2026-08-22 15:48:10 +0300

    Routing
    Farklı klasördeki, aynı isimdeki sayfaların yönlendirme işlemi yapıldı.

---
+++ razorpages/Pages/Employees.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorpages.Pages;
+
+public class EmployeesModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ razorpages/Pages/Employees/Details.scthml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorpages.Pages.Employees;
+
+public class DetailsModel : PageModel
+{
+    
+}
